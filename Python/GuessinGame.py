0c1222fb58248f8003d75eb8714ec9286ce3d28e
Tim Ubial <tim.ubial@gmail.com>
2018-05-25 10:39:13 -0700

    Exception handling for input More feedback to user regarding guesses

--- Python/GuessinGame.py
+++ Python/GuessinGame.py
@@ -2,43 +2,55 @@ import random
 
 win = False # boolean value to keep the game moving until the player wins
 numberGuessed = random.randrange(0, 10001) # sets a random number between 0 and 10000 inclusive
-numberOfGuesses = 1 # used to keep track of the number of guesses
+numberOfGuesses = 0 # used to keep track of the number of guesses
 guess = -1 # variable used to store the user's guess 
 
+print(numberGuessed)
+
 # this is the main loop that your game will play in
-# remember that when the user has 
 while not win:
     # ask the user what their guess is
-    if numberOfGuesses > 0:
-        try:
-            guess = int(input("Guess a number between 0 and 10000: "))
-        except ValueError:
-            print("Please choose an integer.")
-            guess = -1
+    if numberOfGuesses <= 0:
+        guess = input("Guess a number between 0 and 10000 (type e to exit): ")
     else:
-        try:
-            guess = int(input("Try again: "))
-        except ValueError:
-            print("Please choose an integer.")
-            guess = -1
+        guess = input("Try again (type e to exit): ")
+
+    if guess == "e":
+        break
+
+    try:
+        guess = int(guess)
+    except ValueError:
+        print("Please choose an integer.")
+        guess = -1
+        numberOfGuesses -= 1
+
+    numberOfGuesses += 1
 
     difference = abs(numberGuessed - guess)
-    print(difference)
+
     # if statement to see if their guess is correct
-    if guess < 0:
+    if guess == numberGuessed:
+        win = True
+    elif guess < 0:
         pass
     elif guess > numberGuessed:
-        if difference < 50:
+        if  difference < 100:
             print("Getting closer, but still high.")
-        else:
+        elif difference < 50:
+            print("Closer! A little high.")
+        elif difference:
             print("Too high.")
     else:
-        if difference < 50:
+        if difference < 100:
             print("Getting closer, but still low.")
+        elif difference < 50:
+            print("Closer! A little low.")
         else:
             print("Too low.")
 
-# print out how that the user has won
-print("You won!")
-# print out how many guesses it took the user
-print("It only took " + numberOfGuesses + " guesses.")
+if win:
+    # print out how that the user has won
+    print("You won!")
+    # print out how many guesses it took the user
+    print("It only took " + str(numberOfGuesses) + " guesses.")
